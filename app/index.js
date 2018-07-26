c1ac35b13aeca3f6ef1beb90c1baa8bdb6d4150b
mfbieber <mfbieber@gmail.com>
2018-07-26 17:53:59 +0200

    background arcs update on change of rest and worktime.

--- app/index.js
+++ app/index.js
@@ -11,23 +11,30 @@ const buttonPause = document.getElementById("btn-tr");
 const buttonPlay = document.getElementById("btn-br");
 const buttonReset = document.getElementById("btn-bl");
 const buttonForward = document.getElementById("btn-tl");
+const workBackground = document.getElementById("workBackground");
+const restBackground = document.getElementById("restBackground");
 
-let workingTime = 45;
-let restingTime = 15;
+let workingTime;
+let restingTime;
+let remainingWorkTime = workingTime;
+let remainingRestTime = restingTime;
 let i=0;
 let working = false;
 
 workOrRest.text = 'press start!';
 clock.granularity = "seconds";
 clock.ontick = evt => {
+    if (working) {
+        workArc.sweepAngle = 0;
+    }
     timeLeft.text = (268-i).toString() + ' s';
     workArc.sweepAngle=i;
     i++;
-}
+};
 buttonReset.onactivate = evt => {
     working = false;
     workOrRest.text = 'press start!';
-}
+};
 buttonForward.onactivate = evt => {
     if (working) {
         working = false;
@@ -36,32 +43,37 @@ buttonForward.onactivate = evt => {
         working = true;
         workOrRest.text = 'work!';
     }
-}
+};
 buttonPlay.onactivate = evt => {
     if (!working) {
         working = true;
         workOrRest.text = 'work!';
     }
-}
+};
 buttonPause.onactivate = evt => {
     if(working) {
         working = false;
         workOrRest.text = 'rest!';
     }
-}
+};
 
 // Message is received
 messaging.peerSocket.onmessage = evt => {
     console.log(`App received: ${JSON.stringify(evt)}`);
     if (evt.data.key === "backgroundColor" && evt.data.newValue) {
-        let backgroundColor = JSON.parse(evt.data.newValue);
-        background.style.fill = backgroundColor;
+        background.style.fill = JSON.parse(evt.data.newValue);
     }
     if (evt.data.key === "sliderWorking" && evt.data.newValue) {
         workingTime = JSON.parse(evt.data.newValue);
+        workBackground.sweepAngle = (workingTime / (workingTime + restingTime)) * 360 - 2;
+        restBackground.sweepAngle = (restingTime / (workingTime + restingTime)) * 360 - 2;
+        workBackground.startAngle = restBackground.sweepAngle + 4;
     }
     if (evt.data.key === "sliderResting" && evt.data.newValue) {
         restingTime = JSON.parse(evt.data.newValue);
+        workBackground.sweepAngle = (workingTime / (workingTime + restingTime)) * 360 - 2;
+        restBackground.sweepAngle = (restingTime / (workingTime + restingTime)) * 360 - 2;
+        workBackground.startAngle = restBackground.sweepAngle + 4;
     }
     if (evt.data.key === "workArcColor" && evt.data.newValue) {
         let workArcColor = JSON.parse(evt.data.newValue);
