6284890c88d92e7dc19c8a40b3ca34e99a405fe8
mfbieber <mfbieber@gmail.com>
2018-07-26 16:18:11 +0200

    displays arcs, can change colors, set resting and working times, counts down seconds

--- app/index.js
+++ app/index.js
@@ -1,24 +1,58 @@
 import document from "document";
 import * as messaging from "messaging";
+import clock from "clock";
 
 let background = document.getElementById("background");
+const appTitle = document.getElementById("appTitle");
+const workOrRest = document.getElementById("workOrRest");
+const timeLeft = document.getElementById("timeLeft");
+const workArc = document.getElementById("work");
+const restArc = document.getElementById("rest");
+let i=0;
+
+
+clock.granularity = "seconds";
+clock.ontick = evt => {
+    appTitle.text = 'Pomodoro Timer';
+    workOrRest.text = 'work!';
+    timeLeft.text = (268-i).toString() + ' s';
+    workArc.sweepAngle=i;
+    i++;
+}
 
 // Message is received
 messaging.peerSocket.onmessage = evt => {
-  console.log(`App received: ${JSON.stringify(evt)}`);
-  if (evt.data.key === "color" && evt.data.newValue) {
-    let color = JSON.parse(evt.data.newValue);
-    console.log(`Setting background color: ${color}`);
-    background.style.fill = color;
-  }
+    console.log(`App received: ${JSON.stringify(evt)}`);
+    if (evt.data.key === "backgroundColor" && evt.data.newValue) {
+        let backgroundColor = JSON.parse(evt.data.newValue);
+        console.log(`Setting background color: ${backgroundColor}`);
+        background.style.fill = backgroundColor;
+    }
+    if (evt.data.key === "workArcColor" && evt.data.newValue) {
+        let workArcColor = JSON.parse(evt.data.newValue);
+        console.log(`Setting working arc color: ${workArcColor}`);
+        workArc.style.fill = workArcColor;
+    }
+    if (evt.data.key === "restArcColor" && evt.data.newValue) {
+        let restArcColor = JSON.parse(evt.data.newValue);
+        console.log(`Setting resting arc color: ${restArcColor}`);
+        restArc.style.fill = restArcColor;
+    }
+    if (evt.data.key === "textColor" && evt.data.newValue) {
+        let textColor = JSON.parse(evt.data.newValue);
+        console.log(`Setting resting arc color: ${textColor}`);
+        appTitle.style.fill = textColor;
+        workOrRest.style.fill = textColor;
+        timeLeft.style.fill = textColor;
+    }
 };
 
 // Message socket opens
 messaging.peerSocket.onopen = () => {
-  console.log("App Socket Open");
+    console.log("App Socket Open");
 };
 
 // Message socket closes
 messaging.peerSocket.onclose = () => {
-  console.log("App Socket Closed");
+    console.log("App Socket Closed");
 };
--- settings/index.jsx
+++ settings/index.jsx
@@ -1,23 +1,78 @@
 function mySettings(props) {
-  return (
+    return (
     <Page>
       <Section
-        title={<Text bold align="center">Demo Settings</Text>}>
-        <Toggle
-          settingsKey="toggle"
-          label="Toggle Switch"
+        title={<Text bold align="center">Pomodoro Timer Settings</Text>}>
+        <Slider
+          settingsKey="sliderWorking"
+          label={`Work for ${props.settingsStorage.getItem('sliderWorking')} Minutes`}
+          min="0"
+          max="90"
+          onChange={value => props.settingsStorage.setItem('sliderWorking', value)}
         />
+          <Slider
+              settingsKey="sliderResting"
+              label={`Rest for ${props.settingsStorage.getItem('sliderResting')} Minutes`}
+              min="0"
+              max="90"
+              onChange={value => props.settingsStorage.setItem('sliderResting', value)}
+          />
+          <Text>Select background color:</Text>
         <ColorSelect
-          settingsKey="color"
+          settingsKey="backgroundColor"
           colors={[
-            {color: "tomato"},
-            {color: "sandybrown"},
-            {color: "#FFD700"},
-            {color: "#ADFF2F"},
-            {color: "deepskyblue"},
-            {color: "plum"}
+              {color: "#000000"},
+              {color: "#42113b"},
+              {color: "#574f4f"},
+              {color: "#e6ddcd"},
+              {color: "#cadff3"},
+              {color: "#eafbc8"},
+              {color: "#ffffff"}
+          ]}
+          label="Background color"
+        />
+          <Text>Select working arc color:</Text>
+          <ColorSelect
+          label="Working arc color"
+          settingsKey="workArcColor"
+          colors={[
+              {color: "#cce4da"},
+              {color: "#ae2c51"},
+              {color: "#5b787f"},
+              {color: "#143647"},
+              {color: "#d85978"},
+              {color: "#f1cb45"},
+              {color: "#2a6363"}
           ]}
         />
+          <Text>Select resting arc color:</Text>
+          <ColorSelect
+          label="Resting arc color"
+          settingsKey="restArcColor"
+          colors={[
+              {color: "#cb858a"},
+              {color: "#dc8331"},
+              {color: "#abdeb1"},
+              {color: "#c9b386"},
+              {color: "#cce4da"},
+              {color: "#8d9933"},
+              {color: "#459eae"}
+              ]}
+        />
+          <Text>Select text color:</Text>
+          <ColorSelect
+              label="Text color"
+              settingsKey="textColor"
+              colors={[
+                  {color: "#000000"},
+                  {color: "#143647"},
+                  {color: "#574f4f"},
+                  {color: "#5b787f"},
+                  {color: "#87c5b4"},
+                  {color: "#d5ceca"},
+                  {color: "#ffffff"}
+              ]}
+          />
       </Section>
     </Page>
   );
